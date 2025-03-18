bc59fda790114a00d487b232232eb97e6d4e8690
ArtemKosyrev <artemkosyrev93@gmail.com>
2025-03-18 09:45:58 +0500

    feat: (docker) add

--- apps/producer/src/producer.controller.ts
+++ apps/producer/src/producer.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProducerService } from './producer.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 
-@Controller()
+@Controller('orders')
 export class ProducerController {
   constructor(private readonly producerService: ProducerService) {}
 
-  @Post()
+  @Post('place-order')
   async createOrder(@Body() request: CreateOrderDto) {
     return this.producerService.createOrder(request);
   }
